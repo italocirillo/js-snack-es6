c6f16c58e6ffaff5812456134617c880ae097d81
italocirillo <italocirillo@gmail.com>
2023-03-24 18:42:23 +0100

    SNACK 1 COMPLETO

--- Array oggetti bici/js/script.js
+++ Array oggetti bici/js/script.js
@@ -36,17 +36,24 @@ const BiciPesoMinore = {
 }
 
 // SCORRO IL VETTORE DI OGGETTI ASSEGNANDO LA BICI CON IL PESO MINORE
+let stringaAltreBici="";
 biciclette.forEach(element => {
     const {nome, peso} = element;
     if(peso < BiciPesoMinore.peso){
         BiciPesoMinore.peso = peso;
         BiciPesoMinore.nome = nome;
     }
+    // STAMPO LE ALTRE BICI
     console.log(`La bici ${nome} pesa ${peso}`);
+    stringaAltreBici += `<p>La bici ${nome} pesa ${peso}<p>`;
 });
 console.log(`La bici con il peso minore è ${BiciPesoMinore.nome} e pesa ${BiciPesoMinore.peso} kg`);
 // PRENDO L ELEMENTO DALL'HTML E CI METTO IL RISULTATO
 
 const stringaBici=document.getElementById("bici-peso-minore");
+const altreBici=document.getElementById("altre-bici");
 
 stringaBici.innerHTML = `La bici con il peso minore è ${BiciPesoMinore.nome} e pesa ${BiciPesoMinore.peso} kg`;
+altreBici.innerHTML=stringaAltreBici;
+
+
