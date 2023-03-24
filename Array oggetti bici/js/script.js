0240b0672e55eb3b79526f80eaa207f6a1e251f1
italocirillo <italocirillo@gmail.com>
2023-03-24 17:55:29 +0100

    1° esercizio completo

--- Array oggetti bici/js/script.js
+++ Array oggetti bici/js/script.js
@@ -1,1 +1,52 @@
-console.log("hello js");
+// ARRAY OGGETTI BICI
+const biciclette = [
+    {
+        nome: "Atala",
+        peso: 180
+    },
+    {
+        nome: "Filippa",
+        peso: 160
+    },
+    {
+        nome: "Turbo",
+        peso: 240
+    },
+    {
+        nome: "Ciclocross",
+        peso: 80
+    },
+    {
+        nome: "Ebike",
+        peso: 320
+    },
+    {
+        nome: "Gravel",
+        peso: 280
+    },
+    {
+        nome: "Endurance",
+        peso: 120
+    },
+];
+// CREO UN OGGETTO CON LA BICI CHE PESA DI MENO
+const BiciPesoMinore = {
+    nome: "",
+    peso: 10000
+}
+
+// SCORRO IL VETTORE DI OGGETTI ASSEGNANDO LA BICI CON IL PESO MINORE
+biciclette.forEach(element => {
+    const {nome, peso} = element;
+    if(peso < BiciPesoMinore.peso){
+        BiciPesoMinore.peso = peso;
+        BiciPesoMinore.nome = nome;
+    }
+    console.log(`La bici ${nome} pesa ${peso}`);
+});
+console.log(`La bici con il peso minore è ${BiciPesoMinore.nome} e pesa ${BiciPesoMinore.peso} kg`);
+// PRENDO L ELEMENTO DALL'HTML E CI METTO IL RISULTATO
+
+const stringaBici=document.getElementById("bici-peso-minore");
+
+stringaBici.innerHTML = `La bici con il peso minore è ${BiciPesoMinore.nome} e pesa ${BiciPesoMinore.peso} kg`;
